d9cf7f1e752f844a6d99c670d409da0aaf28e8a4
Jeffery05 <jefferyhu2005@gmail.com>
2022-12-30 12:07:33 -0600

    updated app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import About from "./Components/about";
+import Achievements from "./Components/achievements";
+import Projects from "./Components/projects";
+import Home from "./Components/home";
+import Contact from "./Components/contact";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="navbar">
+        <header className="site-header">
+          <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-primary">
+            <div className="container">
+              <a className="navbar-brand" href="home.jsx">
+                {/*<i
+                  className="bi bi-laptop"
+                  role="img"
+                  aria-label="Logo"
+                  style={{padding-right: 5px;}}
+  ></i>*/}
+                Jeffery Hu
+              </a>
+
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+              <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="nav navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="./Components/home.jsx">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="./Components/about.jsx">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="./Components/achievements.jsx">
+                      Acheivements
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="./Components/projects.jsx">
+                      Projects
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link"
+                      href="https://jefferyhu2005.wixsite.com/portfolio/oop-1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Java Tutorials
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="Contact.html">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Achievements" element={<Achievements />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
-}
+};
 
-export default App;
+export default App = () => {
+  return(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
