2a3c5373fdfd71efd340841891d6e94c6ab7fc4f
Jeffery05 <jefferyhu2005@gmail.com>
2022-12-31 11:42:14 -0600

    added contact page

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,12 +1,141 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default function Contact() {
-    return (
-      <div className="about">
-        <h2>About Us</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+  return (
+    <div>
+      <div className="container mt-8">
+        <h1 className="mb-3 ps-4">Contact Me!</h1>
+
+        <form
+          className="needs-validation"
+          action="https://formsubmit.co/21cf21a5b8ab56a75a57936bb6cf5c64"
+          method="POST"
+          enctype="multipart/form-data"
+        >
+          <div className="row g-3">
+            <div className="col-sm-6">
+              <label for="firstName" className="form-label">
+                First Name
+              </label>
+              <input
+                name="First name"
+                type="text"
+                className="form-control"
+                id="firstName"
+                placeholder="First Name"
+                value=""
+                required
+              />
+              <div className="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <label for="lastName" className="form-label">
+                Last Name
+              </label>
+              <input
+                name="Last name"
+                type="text"
+                className="form-control"
+                id="firstName"
+                placeholder="Last Name"
+                value=""
+                required
+              />
+              <div className="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+            <div className="col-sm-12">
+              <label for="email" className="form-label">
+                Email (Optional)
+              </label>
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="you@example.com"
+              />
+            </div>
+            <div className="col-sm-12">
+              <label for="phone" className="form-label">
+                Phone Number (Optional)
+              </label>
+              <input
+                name="phone"
+                type="tel"
+                className="form-control"
+                id="phone"
+                placeholder="123-456-7890"
+              />
+            </div>
+            <div className="col-sm-12">
+              <label for="commentTextArea" className="form-label">
+                Comments/Inquires
+              </label>
+              <textarea
+                name="message"
+                className="form-control"
+                id="commentTextArea"
+                rows="10"
+                required
+              ></textarea>
+              <div className="invalid-feedback">
+                Valid comment/question is required.
+              </div>
+            </div>
+            <div className="col-sm-12">
+              <label for="uploadFile" className="form-label">
+                Upload File (Optional)
+              </label>
+              <input
+                name="attachment"
+                accept="image/png, image/jpeg"
+                className="form-control"
+                type="file"
+                id="uploadFile"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary mt-3"
+            id="sendMessage"
+          >
+            Submit
+          </button>
+          <input
+            type="hidden"
+            name="_next"
+            value="https://jeffery05.github.io/Website/done.html"
+          />
+        </form>
+        <div className="toast position-fixed bottom-0 start-50 translate-middle-x mb-3">
+          <div
+            id="sendMessage"
+            className="toast hide"
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
+          >
+            <div className="toast-header">
+              <strong className="me-auto">Jeffery</strong>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="toast"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="toast-body">
+              Thank you for submitting your message. We will get back in touch
+              soon!
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
