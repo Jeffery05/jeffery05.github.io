7131fcc7149646517f420ba40d678f879bd3907b
Jeffery05 <jefferyhu2005@gmail.com>
2022-12-30 18:42:22 -0600

    Updated home page and routed all pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import Home from "./Components/home";
 import About from "./Components/about";
+import Achievements from "./Components/achievements";
+import Projects from "./Components/projects";
+import Contact from "./Components/contact";
 
 function App(){
   return (
@@ -10,12 +13,18 @@ function App(){
         <h1>Jeffery Hu</h1>
         <NavLink to="/">Home</NavLink>
         <NavLink to="about">About</NavLink>
+        <NavLink to="achievements">Achievements</NavLink>
+        <NavLink to="projects">Projects</NavLink>
+        <NavLink to="contact">Contact</NavLink>
       </nav>
     </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="about" element={<About />}/>
+          <Route path="achievements" element={<Achievements />}/>
+          <Route path="projects" element={<Projects />}/>
+          <Route path="contact" element={<Contact />}/>
         </Routes>
       </main>
     </BrowserRouter>
--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 export default function Home() {
   return (
-    <div className="container-fluid jumbotron jumbotron-welcome py-5" style={{backgroundColor: "black"}}>
+    <div className="container-fluid jumbotron jumbotron-welcome py-5">
       <div className="container">
-        <h1 className="h1-Welcome">
-          WELCOME!
-        </h1>
+        <row>
+          <h1 className="h1-Welcome"><strong>JEFFERY HU</strong></h1>
+        </row>
+        <row>
+          <h3>I am a </h3>
+            <div class="slider">
+              <div class="slider-t1">Leader</div>
+              <div class="slider-t2">Problem Solver</div>
+              <div class="slider-t2">Lifelong Learner</div>
+              <div class="slider-t2">Risktaker</div>
+              <div class="slider-t2">Teammate</div>
+            </div>
+        </row>
       </div>
     </div>
   );
