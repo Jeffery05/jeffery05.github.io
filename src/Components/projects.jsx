8e1583eacb4d6d66f35a3f04ae04e1f31897abba
Jeffery05 <jefferyhu2005@gmail.com>
2023-01-02 20:30:20 -0600

    grammar edits

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -20,7 +20,7 @@ export default function Projects() {
           <div className="card-body">
             <h3 className="card-title"><b>trashdash</b></h3>
             <p className="card-text text-left">
-            To deal with the problem of litter, this web application encourages citizen led environmental cleanups! It does this by introducing a centralized litter database, where citizens can post about areas with high levels of trash so it can be cleaned up during community pickup days! As an extra incentive, users gain points whenever they report/resolve litter piles, which can be exchanged for donations to a charity of their choice!.
+            To deal with the problem of litter, this web application encourages citizen-led environmental cleanups! It does this by introducing a centralized litter database, where citizens can post about areas with high levels of trash so it can be cleaned up during community pickup days. As an extra incentive, users gain points whenever they report/resolve litter piles, which can be exchanged for donations to a charity of their choice!
             </p>
           </div>
           <div className="card-footer">
